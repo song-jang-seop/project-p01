6d56ca98669548a235fb4c3ee2f71637312b1728
song <shoushou410@gmail.com>
2022-11-17 22:58:04 +0900

    Customer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customer = [{
+    'id': 1,
+    'image': 'https://placeimg.com/164/164/1',
+    'name': '홍길동1',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+},{
+    'id': 2,
+    'image': 'https://placeimg.com/164/164/2',
+    'name': '홍길동2',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+},{
+    'id': 3,
+    'image': 'https://placeimg.com/164/164/3',
+    'name': '홍길동3',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+}
+
+]
 
 class App extends Component {
     render() {
         return (
-            <div className="gray-background">
-                <img src={logo} lat="logo" alt="logo"/>
-                <h2>KB Admin</h2>
+            <div>
+                {
+                    customer.map(c => {
+                        return(
+                            <Customer
+                            key={c.id}
+                            id={c.id}
+                            image={c.image}
+                            name={c.name}
+                            birthday={c.birthday}
+                            gender={c.gender}
+                            job={c.job}
+                            />
+                        );
+                    }
+                    )
+                }
             </div>
+
         );
     }
 }
