9866ca588a1379907272799743f601a8d61675a6
song <shoushou410@gmail.com>
2022-11-21 23:48:28 +0900

    .

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -30,7 +30,7 @@ class CustomerAdd extends React.Component {
         })
     }
 
-    handleValueChage = (e) => {
+    handleValueChange = (e) => {
         let nextState = {};
         nextState[e.target.name] = e.target.value;
         this.setState(nextState);
@@ -56,7 +56,7 @@ class CustomerAdd extends React.Component {
         return (
             <form onSubmit={this.handleFormSubmit}>
                 <h1>고객 추가</h1>
-                프로필 이미지: <input type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
+                프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
                 이름: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
                 생년월일: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChage}/><br/>
                 성별: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChage}/><br/>
